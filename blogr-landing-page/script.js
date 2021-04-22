4e7524d8b237baebd5c23c9edc92a165579df5b0
davidtan <developer.davidtan@gmail.com>
2021-04-22 20:00:19 +0800

    clean up the prototype

--- blogr-landing-page/script.js
+++ blogr-landing-page/script.js
@@ -1,9 +1,9 @@
-const togglerBtn = document.getElementById('js-toggler-btn');
+const togglerBtn = document.getElementById('menu-toggler');
 
 togglerBtn.addEventListener('click', function () {
     const btnImage = this.getElementsByTagName('img')[0];
     const isCurrentExpanded = btnImage.getAttribute('aria-expanded') == "true";
-    const modelElementId = document.getElementById(this.dataset.target);
+    const modelElementId = document.querySelector(this.getAttribute('data-js-target'));
     const shouldExpand = !isCurrentExpanded;
 
     btnImage.setAttribute("aria-expanded", shouldExpand.toString());
@@ -13,20 +13,36 @@ togglerBtn.addEventListener('click', function () {
         : modelElementId.classList.add('collapse');
 });
 
-const accordionElements = document.querySelectorAll(".js-header-nav-accordion");
+
+const collapsePrevExpandedElements = (currentElement) => {
+    const prevExpandedElements = document.querySelectorAll("#menu-accordion [data-js-toggle='collapse'][aria-expanded='true']");
+    prevExpandedElements.forEach(function (element) {
+        if (element !== currentElement) {
+            toggle(element);
+        }
+    });
+}
+
+const toggle = (element) => {
+    const icon = element.querySelector("img[data-js-icon='collapse']");
+    const isCurrentExpanded = element.getAttribute('aria-expanded') == "true";
+    const shouldExpand = !isCurrentExpanded;
+    const toggleBodyElement = document.querySelector(element.getAttribute('data-js-target'));
+
+    element.setAttribute("aria-expanded", shouldExpand.toString());
+    shouldExpand
+        ? icon.classList.add('rotate-180')
+        : icon.classList.remove('rotate-180');
+    shouldExpand
+        ? toggleBodyElement.classList.remove('collapse')
+        : toggleBodyElement.classList.add('collapse');
+}
+
+const accordionElements = document.querySelectorAll("#menu-accordion [data-js-toggle='collapse']");
 
 accordionElements.forEach(function (element) {
     element.addEventListener("click", function () {
-        const icon = this.getElementsByTagName('img')[0];
-        const isCurrentExpanded = icon.getAttribute('aria-expanded') == "true";
-        const shouldExpand = !isCurrentExpanded;
-
-        icon.setAttribute("aria-expanded", shouldExpand.toString());
-        shouldExpand
-            ? icon.classList.add('rotate-180')
-            : icon.classList.remove('rotate-180');
-        shouldExpand
-            ? this.nextElementSibling.classList.remove('collapse')
-            : this.nextElementSibling.classList.add('collapse');
+        collapsePrevExpandedElements(this);
+        toggle(this);
     });
 });
